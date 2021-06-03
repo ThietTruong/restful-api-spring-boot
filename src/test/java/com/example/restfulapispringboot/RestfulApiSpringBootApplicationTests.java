de53999b8de7ac6a326ff662989707ee7ca6acf3
project1 <thiettruong1999@gmail.com>
2021-06-03 14:01:26 +0700

    project initialization

---
+++ src/test/java/com/example/restfulapispringboot/RestfulApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
